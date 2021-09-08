09f05a53fdd019035bbc2f33ac4a09278e1511c9
harshitScript <85029089+harshitScript@users.noreply.github.com>
2021-09-08 21:43:39 +0530

    links bugs fixed

--- src/Components/HomePageComponents/MainBody/MainBodyComponents/LogoAndImportantLinks/ImpLinkCard.js
+++ src/Components/HomePageComponents/MainBody/MainBodyComponents/LogoAndImportantLinks/ImpLinkCard.js
@@ -1,9 +1,15 @@
-import styles from './ImpLinkCard.module.css'
-import Card from '../../../../../UI/Card'
-const ImpLinkCard = props =>{
-    return <Card className={styles.linkCard}>
-    <span>{`${props.title} : `}<a href={props.href}>Click</a></span>
-    
-  </Card>
-}
+import styles from "./ImpLinkCard.module.css";
+import Card from "../../../../../UI/Card";
+const ImpLinkCard = (props) => {
+  return (
+    <Card className={styles.linkCard}>
+      <span>
+        {`${props.title} : `}
+        <a href={props.href} target="_blank" rel="noreferrer">
+          Click..
+        </a>
+      </span>
+    </Card>
+  );
+};
 export default ImpLinkCard;
