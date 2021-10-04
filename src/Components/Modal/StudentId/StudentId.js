9a5eba5835f3b21d325fc5e4d09d75770dc90b67
harshitScript <85029089+harshitScript@users.noreply.github.com>
2021-10-04 14:46:41 +0530

    pre update commit

--- src/Components/Modal/StudentId/StudentId.js
+++ src/Components/Modal/StudentId/StudentId.js
@@ -1,6 +1,12 @@
 import { useSelector } from "react-redux";
+import { FiClipboard } from "react-icons/fi";
+import { FaClipboardCheck } from "react-icons/fa";
 import styles from "./StudentId.module.css";
+import { useState } from "react";
+
 const StudentId = (props) => {
+  const [copied, setCopied] = useState(false);
+
   const studentIdToDisplayWhenNewUserIsAdded = useSelector(
     (state) => state.modal.additionalPayload?.studentId
   );
@@ -8,6 +14,18 @@ const StudentId = (props) => {
   const studentIdToDisplayWhenAuthenticatedUserIsRequesting = useSelector(
     (state) => state.userData.activeStudentId
   );
+
+  // COPY TO CLIPBOARD FUNCTIONALITY
+  const copyToClipboardHandler = () => {
+    if (studentIdToDisplayWhenNewUserIsAdded) {
+      navigator.clipboard.writeText(studentIdToDisplayWhenNewUserIsAdded);
+    } else {
+      navigator.clipboard.writeText(
+        studentIdToDisplayWhenAuthenticatedUserIsRequesting
+      );
+    }
+    setCopied(true);
+  };
 
   let studentIdToDisplay;
   if (studentIdToDisplayWhenNewUserIsAdded) {
@@ -20,7 +38,22 @@ const StudentId = (props) => {
   return (
     <>
       <span className={styles.firstSpan}>Student Id</span>
-      <span className={styles.secondSpan}>{`'${studentIdToDisplay}'`}</span>
+      <span className={styles.secondSpan}>
+        {`'${studentIdToDisplay}' `}
+        {!copied && (
+          <abbr title="Copy to clipboard">
+            <FiClipboard
+              onClick={copyToClipboardHandler}
+              className={styles.warningClass}
+            />
+          </abbr>
+        )}
+        {copied && (
+          <abbr title="Copied">
+            <FaClipboardCheck className={styles.successClass} />
+          </abbr>
+        )}
+      </span>
       <span className={styles.thirdSpan}>
         *Keep it safe for future reference.*
       </span>
